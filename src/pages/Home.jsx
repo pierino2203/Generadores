4ac0d0e8253e2e849b77bfa29b05eb6afa34e8c0
pierino2203 <pierinoe@gmail.com>
2025-04-17 00:16:13 -0300

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Generacion de numeros pseudoaleatorios</h1>
+            <h2>Elija el metodo de Generacion:</h2>
+            <button>Lineal</button>
+            <button>Cuadratico</button>
+            <button>Multiplicativo</button>
+            <button>Congruencial Mixto</button>
+        </div>
+    )
+} 
